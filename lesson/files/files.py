b9f54d2f13aae994cb21cc4a1cfc9965de43218c
harunlakodla <harunlakodla@gmail.com>
2022-11-02 10:28:34 +0300

    added read function inside files.py

--- lesson/files/files.py
+++ lesson/files/files.py
@@ -16,3 +16,47 @@ f.close()
 with open("test.txt","r") as f:
     print("file is open")
     print(type(f))
+
+####################################################
+f=open("test.txt","w")
+
+f.write("this is a test \n")
+f.write("this is a second test \n")
+
+f.close()
+
+with open("test.txt","w") as f:
+    for i in range(10):
+        f.write("Nums : {}".format(i))
+
+f.close()
+
+with open("test.txt","a+") as f:
+    f.write("addition operation \n")
+    for i in range(10):
+        f.write(i)
+############################
+
+
+with open("test.txt","w") as f:
+    
+    while True:
+        textVar =input("Give a word")
+        if textVar=="exit":
+            break
+        f.write(textVar+"\n")
+        
+###################################
+
+with open("test.txt","r+") as f:
+    s =f.read(6) #number of chraracters needs to be read from file
+    print(s)
+    
+#############################################
+
+with open("main.py","r+") as f:
+    while True:
+        s =f.read(5)
+        if s=="":
+            break
+        print(s,end="")
