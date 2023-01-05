9d5d3aa994fd1cdbd75144493a9fefdd99f69477
harunlakodla <harunlakodla@gmail.com>
2023-01-06 00:43:00 +0300

    update files

---
+++ lesson/tupples/main.py
@@ -0,0 +1,6 @@
+
+tuple1=(1,2,3,4,5,6,3,2,1,1)
+tuple2=("x","y","z","d","e")
+
+print("count ",tuple1.count(0))
+print("count ",tuple2.count("x"))
---
+++ list/main.py
@@ -0,0 +1,8 @@
+
+
+mylist1=[5,6,7]
+mylist2=[1,2,3]
+
+mylist1.extend(mylist2)
+
+print(mylist1)
